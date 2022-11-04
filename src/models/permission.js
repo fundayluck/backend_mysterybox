5de8442728e4f240e7c36261deb28586d46fb074
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2022-11-04 17:49:14 +0700

    removed some file

--- src/models/permission.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const permissionSchema = new mongoose.Schema({
-    nama: {
-        type: String
-    }
-})
-
-mongoose.model("permission", permissionSchema)
